644fa7672c46482c1d27fe879ddebaee7270f443
FelipeCMBarros <felipembarros64@gmail.com>
2026-09-08 16:12:30 -0300

    or16.vhd feito

---
+++ b_logComb/src/or16.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity or16 is
+	port ( 
+			a:   in  STD_LOGIC_VECTOR(15 downto 0);
+			b:   in  STD_LOGIC_VECTOR(15 downto 0);
+			q:   out STD_LOGIC_VECTOR(15 downto 0));
+end entity;
+
+architecture arch of or16 is
+begin
+
+    gen_or : for i in 0 to 15 generate
+        S(i) <= A(i) or B(i);
+    end generate gen_or;
+
+end architecture;
