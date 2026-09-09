a57bff78b98626599af1b33054a19072b65c1d21
FelipeCMBarros <felipembarros64@gmail.com>
2026-09-09 14:36:28 -0300

    mux8way16.vhd 100% pronto

--- b_logComb/src/detectordemoedas.vhd
+++ b_logComb/src/detectordemoedas.vhd
@@ -15,15 +15,18 @@ architecture Behavioral of detectordemoedas is
 begin
 
 process(q, d, n)
+
 begin
-    case (q & d & n) is        
+    case (q & d & n) is     
+
         when "000" => cents <= "00000";
         when "001" => cents <= "00101";
         when "010" => cents <= "01010";
         when "100" => cents <= "11001";    
-        when others =>
-        cents <= "00000";        
+        when others =>cents <= "00000";        
+
         end case;
+
     end process;
 
 end Behavioral;
