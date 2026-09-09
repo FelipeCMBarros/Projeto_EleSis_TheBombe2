99bc986ff2943379e23dac7d0466e3d08e8c94b0
FelipeCMBarros <felipembarros64@gmail.com>
2026-09-09 11:24:09 -0300

    detectordemoedas.vhd criado e feito.

---
+++ b_logComb/src/detectordemoedas.vhd
@@ -0,0 +1,29 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity detectordemoedas is
+    Port (
+        q : in STD_LOGIC;
+        d : in STD_LOGIC;
+        d : in STD_LOGIC;
+        
+        cents : out STD_LOGIC_VECTOR(4 downto 0)
+    );
+end detectordemoedas;
+
+architecture Behavioral of detectordemoedas is
+begin
+
+process(q, d, n)
+begin
+    case (q & d & n) is        
+        when "000" => cents <= "00000";
+        when "001" => cents <= "00101";
+        when "010" => cents <= "01010";
+        when "100" => cents <= "11001";    
+        when others =>
+        cents <= "00000";        
+        end case;
+    end process;
+
+end Behavioral;
