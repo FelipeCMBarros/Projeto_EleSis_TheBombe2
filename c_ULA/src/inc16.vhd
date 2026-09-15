3cd366bd78689dfab2fa0bfdb27537aff358ee1e
FelipeCMBarros <felipembarros64@gmail.com>
2026-09-15 16:22:49 -0300

    inc16.vhd corrigido (versao 12)

--- c_ULA/src/inc16.vhd
+++ c_ULA/src/inc16.vhd
@@ -1,35 +1,43 @@
--- Elementos de Sistemas
--- by Luciano Soares
--- Inc16.vhd
-
--- Incrementador de 16 bits
--- adiciona 1 ao valore de entrada (adição aritmética)
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
 entity Inc16 is
-	port(
-		a   :  in STD_LOGIC_VECTOR(15 downto 0);
-		q   : out STD_LOGIC_VECTOR(15 downto 0)
-	);
+    port(
+        a : in  STD_LOGIC_VECTOR(15 downto 0);
+        q : out STD_LOGIC_VECTOR(15 downto 0)
+    );
 end entity;
 
 architecture rtl of Inc16 is
-  -- Aqui declaramos sinais (fios auxiliares)
-  -- e componentes (outros módulos) que serao
-  -- utilizados nesse modulo.
 
-  component Add16 is
-    port(
-      a   :  in STD_LOGIC_VECTOR(15 downto 0);
-      b   :  in STD_LOGIC_VECTOR(15 downto 0);
-      q   : out STD_LOGIC_VECTOR(15 downto 0)
-      );
-  end component;
+    component FullAdder is
+        port(
+            a, b, c : in  STD_LOGIC;
+            soma    : out STD_LOGIC;
+            vaium   : out STD_LOGIC
+        );
+    end component;
+
+    signal carry : STD_LOGIC_VECTOR(16 downto 0);
+
+    constant ONE : STD_LOGIC_VECTOR(15 downto 0) :=
+        "0000000000000001";
 
 begin
-  a => a(i),
-  b => '1',
-  q => sum(i),
+
+    carry(0) <= '0';
+
+    gen_adders : for i in 0 to 15 generate
+
+        FA : FullAdder
+            port map(
+                a     => a(i),
+                b     => ONE(i),
+                c     => carry(i),
+                soma  => q(i),
+                vaium => carry(i + 1)
+            );
+
+    end generate;
+
 end architecture;
