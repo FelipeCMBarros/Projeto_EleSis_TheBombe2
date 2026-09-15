fe4c0ddb07dc427924dfca13641aaf08c9e69e82
FelipeCMBarros <felipembarros64@gmail.com>
2026-09-15 15:59:26 -0300

    add16.vhd corrigido (versao 3)

--- c_ULA/src/add16.vhd
+++ c_ULA/src/add16.vhd
@@ -29,19 +29,6 @@ architecture rtl of Add16 is
   end component;
 
 begin
-  -- Implementação vem aqui!
-
-  carry(0) <= '0';
-
-    GEN_FULL_ADDERS: for i in 0 to 15 generate
-
-        FA: entity work.fulladder
-            port map (
-                a => a(i),
-                b => b(i),
-                q => sum(i),
-            );
-
-    end generate GEN_FULL_ADDERS;
+  q => std_logic_vector(unsigned(a) + unsigned(b))
 
 end architecture;
